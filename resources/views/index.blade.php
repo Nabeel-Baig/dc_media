713ea413112d7d6e836d9e64d0f3d9703c542675
MuhammadAbdullahMeer1 <muhammad.abdullah@shottechnologies.co>
2022-02-12 04:24:50 +0500

    frontend changes at index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,17 +52,6 @@ toastr.success("{{ session('message') }}");
             </div>
         </div>
     </div>
-    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-header">
-            <img src="..." class="rounded me-2" alt="...">
-            <strong class="me-auto">Bootstrap</strong>
-            <small>11 mins ago</small>
-            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-        </div>
-        <div class="toast-body">
-            Hello, world! This is a toast message.
-        </div>
-    </div>
     <div class="col-xl-8">
         <div class="row">
             <div class="col-md-4">
